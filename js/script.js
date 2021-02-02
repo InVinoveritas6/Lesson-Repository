aaffc5938d89a5386213846b5e25f103e1a151b8
Sasha <sagodich@mail.ru>
2021-02-02 14:17:07 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+"use strict";
+
+//.....Lesson №2
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+
+// console.log(number);
+
+//.....Lesson №3
+
+// const obj = {
+//    name: "John",
+//    age: 25,
+//    isVarried: false
+// }
+// console.log(obj["name"]);
+
+//.....Lesson №4
+
+//  const res = confirm('hello');
+//  console.log(res);
+
+// const answ = prompt("123", "1");
+// console.log(answ);
+
+// const answers = [];
+
+// answers[0] = prompt('как ваше имя', '');
+// answers[1] = prompt('как ваше имя', '');
+// answers[2] = prompt('как ваше имя', '');
+
+// document.write(answers)
+
+//.....Lesson №5
+
+//  let incr = 10,
+//      decr = 10;
+// //  ++incr;
+// //  --decr;
+
+//  console.log(++incr);
+//  console.log(--decr);
+
+//.....Lesson №6
+ 
+// console.log(4%2);
+
+// const isChecked = true,
+//       isClose = true;
+// let Nba = isChecked && isClose;
+// if (Nba==true){
+//    alert('Hello')
+// };
+// console.log(Nba);
+
+//.....Lesson №7
+
