a7cdcfcc4d57698508c044d9635b3f02bcd1eecb
Sasha <sagodich@mail.ru>
2021-02-03 18:50:32 +0300

    Lesson14

--- js/script.js
+++ js/script.js
@@ -158,3 +158,35 @@
 // const calc = (a, b) => {return a + b};
 
 //.................................................Lesson №14............................................
+
+// const str = "tEst";
+// const arr = [1, 2, 3];
+
+// console.log(str[2] ='b');
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// console.log(str);
+
+// const fruit = "Some fruit";
+
+// console.log(fruit.indexOf("fruit"));
+
+// const logg = "Hello word";
+
+// console.log(logg.slice(6, 11));
+
+// console.log(logg.substring(6, 11));
+
+// console.log(logg.substr(6, 5));
+
+// const num = 12.2;
+
+// console.log(Math.round(num));
+
+// const test = "12.2px";
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
+
+//.................................................Lesson №16............................................
