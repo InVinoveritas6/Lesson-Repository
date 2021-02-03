adf22e1fbb5729b1e67a7b1fb25cc3e41f70bc06
Sasha <sagodich@mail.ru>
2021-02-03 13:25:16 +0300

    Lesson 11

--- js/script.js
+++ js/script.js
@@ -92,4 +92,31 @@
 //    default:
 //       console.log('No');
 //       break;
+//
+//.....Lesson №11
+
+// let num = 50;
+
+// while(num <= 55) {
+//    console.log(num);
+//    num++;
 // }
+
+// do {
+//    console.log(num);
+//    num++;
+// }
+// while(num <= 55);
+
+// for(let i=1; i<10; i++){
+//    console.log(num);
+//    num++
+// }
+
+// for(let i=1; i<10; i++){
+//    if(i===10){
+//       break;
+//    }
+//    console.log(i);
+// }
+
