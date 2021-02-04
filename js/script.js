ec19abd6f5fbba01cfa3c1d8bca34371d07ba2a6
Sasha <sagodich@mail.ru>
2021-02-04 20:29:12 +0300

    Lesson19

--- js/script.js
+++ js/script.js
@@ -293,4 +293,108 @@
 
 //.................................................Lesson №19............................................
 
+// let a = 5,
+//    b = a;
+// b = b + 5;
 
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//    a: 5,
+//    b: 1
+// };
+
+// const copy = obj;
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+// function copy(maunObj) {
+//    let objCopy = {};
+
+//    let key;
+//    for (key in maunObj) {
+//       objCopy[key] = maunObj[key];
+//    }
+
+//    return objCopy;
+// }
+
+// const numbers = {
+//    a: 2,
+//    b: 5,
+//    c: {
+//       x: 7,
+//       y: 4
+//    }
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 50;
+
+// console.log(newNumbers);
+// console.log(numbers);
+
+// const add = {
+//    d: 17,
+//    e: 20
+// };
+// const clone = Object.assign({}, add);
+
+// clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+// const oldArr = ['a', 'b', 'c', 'd'];
+// const newArr = oldArr.slice();
+
+// newArr[1] = 'ghghgh';
+
+// console.log(newArr);
+// console.log(oldArr);
+
+// const video = ['youtube', 'vimeo', 'rutube'],
+//    blogs = ['wordpress', 'livejournal', 'blogger'],
+//    internet = [...video, ...blogs, 'vk', 'facebook'];
+
+// console.log(internet);
+
+// function log(a, b, c) {
+//    console.log(a);
+//    console.log(b);
+//    console.log(c);
+// }
+
+// const num = [3, 5, 7];
+
+// log(...num);
+
+// const array = ["a", "b"];
+
+// const newArrr = [...array];
+
+// newArrr[1] = 'c';
+
+// console.log(array);
+// console.log(newArrr);
+
+// const q = {
+//    one: 1,
+//    two: 2
+// };
+
+// const nweObj = {
+//    ...q
+// };
+
+// nweObj.two = 3;
+
+// console.log(nweObj);
+// console.log(q);
+
+//.................................................Lesson №20............................................
