783a4fd737b489516426259f99ba35cb53a5c3a4
Sasha <sagodich@mail.ru>
2021-02-08 13:51:23 +0300

    Lesson20

--- js/script.js
+++ js/script.js
@@ -398,3 +398,33 @@
 // console.log(q);
 
 //.................................................Lesson №20............................................
+
+// let str = "some";
+// let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+// console.dir([1,2,3]);
+
+// const soldier = {
+//    health: 400,
+//    aromor: 100,
+//    sayHello: function() {
+//       console.log('Hello');
+//    }
+// };
+
+// const john = Object.create(soldier);
+
+// const john = {
+//    health: 100
+// };
+
+// john.__proto__ = soldier;
+
+// Object.setPrototypeOf(john, soldier);
+
+// console.log(john);
+
+// john.sayHello();
